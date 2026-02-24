c2119c05bb7b873e22c93da1204083910a20e159
Cristian Sánchez Pineda <harmaharcri@hotmail.com>
2026-02-23 23:08:53 -0500

    Add medieval implementations

---
+++ mythos-rpg/src/model/medieval/medieval_settlement.py
@@ -0,0 +1,5 @@
+from settlement import Settlement
+
+class MedievalCastle(Settlement):
+    def __init__(self, name, defense_level):
+        super().__init__(name, defense_level)
---
+++ mythos-rpg/src/model/medieval/medieval_world.py
@@ -0,0 +1,6 @@
+
+from world import World
+
+class MedievalWorld(World):
+    def __init__(self, name):
+        super().__init__(name)
