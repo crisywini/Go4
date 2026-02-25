89c6db5c19958295bcd9fdc857af1d02420c6a72
Cristian Sánchez Pineda <harmaharcri@hotmail.com>
2026-02-25 00:00:19 -0500

    Add MiseEnPlace swift project for builder pattern

---
+++ MiseEnPlace/MiseEnPlaceTests/MiseEnPlaceTests.swift
@@ -0,0 +1,17 @@
+//
+//  MiseEnPlaceTests.swift
+//  MiseEnPlaceTests
+//
+//  Created by Cristian Sánchez Pineda on 24/02/26.
+//
+
+import Testing
+@testable import MiseEnPlace
+
+struct MiseEnPlaceTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
